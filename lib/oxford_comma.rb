e97610e2afb6500014a937eb98e9f7adc47a8f53
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-20 03:22:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join 
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3
+    array.join(" , ")
+    array.each_with_index {|o, i| string[o]=replacments[i]}
+end
 end
